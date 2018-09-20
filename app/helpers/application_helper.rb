a4ac41bc72fbe00e183f3e29697f2d4055f1d4da
Stef Egbert <stephenegbert@gmail.com>
2018-09-20 17:23:51 -0400

    image path fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
   def randomized_background_image
-  images = ["assets/0.jpg", "assets/1.jpg", "assets/2.jpg", "assets/3.jpg", "assets/4.jpg","assets/5.jpg","assets/6.jpg","assets/7.jpg"]
+  images = ["/images/0.jpg", "/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg","/images/5.jpg","/images/6.jpg","/images/7.jpg"]
   images[rand(images.size)]
 end
 
 def signup_background_image
-images = ["assets/s1.jpg",  "assets/s3.jpg", "assets/s4.jpg", "assets/s5.jpg","assets/s6.jpg","assets/s7.jpg"]
+images = ["/images/s1.jpg",  "/images/s3.jpg", "/images/s4.jpg", "/images/s5.jpg","/images/s6.jpg","/images/s7.jpg"]
 images[rand(images.size)]
 end
 
 def edu_background_image
-images = ["assets/e1.jpg",  "assets/e2.jpg", "assets/e3.jpg"]
+images = ["/images/e1.jpg",  "/images/e2.jpg", "/images/e3.jpg"]
 images[rand(images.size)]
 end
 
